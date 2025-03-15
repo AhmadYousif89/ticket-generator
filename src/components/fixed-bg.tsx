bbc8cdca9c3c53dbcf4b4fd4c6c829c071db147c
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-03-15 03:49:02 +0200

    Add fixed backgrounds to component

---
+++ src/components/fixed-bg.tsx
@@ -0,0 +1,39 @@
+import bgLines from "../assets/images/pattern-lines.svg";
+import circles from "../assets/images/pattern-circle.svg";
+import squigglyLine from "../assets/images/pattern-squiggly-line-top.svg";
+import squigglyLines from "../assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg";
+import squigglyLinesDesktop from "../assets/images/pattern-squiggly-line-bottom-desktop.svg";
+
+export default function FixedBackgrounds() {
+  return (
+    <>
+      <img
+        src={bgLines}
+        className="absolute top-0 -z-10 mx-auto size-full max-w-[1440px] object-cover object-left"
+      />
+
+      <img
+        src={squigglyLine}
+        alt="squiggly line pattern"
+        className="absolute -top-4 right-0 -z-10 size-32 lg:right-16 lg:size-52 xl:size-80"
+      />
+      <img
+        src={circles}
+        alt="circle pattern"
+        className="absolute -top-8 -left-5 -z-10 size-28 lg:size-44"
+      />
+      <img
+        src={circles}
+        alt="circle pattern"
+        className="fixed top-1/2 -right-10 -z-10 size-32 lg:absolute lg:right-16 lg:size-44"
+      />
+      <img
+        src={squigglyLines}
+        srcSet={`${squigglyLines} 768w, ${squigglyLinesDesktop} 1024w`}
+        sizes="(max-width: 1023px) 768px, 1024px"
+        alt="squiggly line bottom pattern"
+        className="absolute -bottom-0 -z-10"
+      />
+    </>
+  );
+}
